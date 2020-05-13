28d783e5a1de911e9efcae3bcb4cc5d1038232e2
Ruki Fujiwara <rukiwillgotoksf@gmail.com>
2020-05-14 02:43:37 +0900

    終わったかな？！

---
+++ Wordlist/ListTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  ListTableViewCell.swift
+//  Wordlist
+//
+//  Created by 藤原琉暉 on 2020/05/11.
+//  Copyright © 2020 fujiwara lupinus. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet var englishLabel:UILabel!
+    @IBOutlet var japaneseLabel:UILabel!
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
